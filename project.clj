12a399ba0876a381a9693f5872973ee96d04e232
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2014-08-26 21:54:04 +0300

    github link

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.danielsz/cljs-utils "0.1.0-SNAPSHOT"
-  :description "General purpose utils for clojurescript development"
-  :url "http://example.com/FIXME"
+  :description "General purpose utils for clojurescript development."
+  :url "https://github.com/danielsz/cljs-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :scm {:name "git"
-        :url "https://github.com/danielsz/system"})
+        :url "https://github.com/danielsz/cljs-utils"})
