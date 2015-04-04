4ae80834e611a7115ccc8629a2d878149fe65dcc
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-04-04 13:19:47 +0300

    read-edn

--- src/cljs_utils/core.clj
+++ src/cljs_utils/core.clj
@@ -1,4 +1,8 @@
-(ns cljs-utils.core)
+(ns cljs-utils.core
+  (:require [clojure.edn]))
 
 (defmacro read-file [uri]
   (slurp uri))
+
+(defmacro read-edn [string]
+  (clojure.edn/read-string string))
