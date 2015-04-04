924b57f578c1a4b58ac5e38e273c06cd5307fd99
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-04-04 17:55:09 +0300

    glob

--- src/cljs_utils/core.clj
+++ src/cljs_utils/core.clj
@@ -1,6 +1,14 @@
-(ns cljs-utils.core)
+(ns cljs-utils.core
+  (:require [clojure.java.io :refer [file]]))
 
 (defmacro read-file [uri]
   (slurp uri))
 
+(defn walk [dirpath pattern]
+  (doall (filter #(re-matches pattern (.getName %))
+                 (file-seq (file dirpath)))))
+
+;; (map #(println (.getPath %)) (walk "src" #".*\.clj"))
+(defmacro glob [root glob]
+  (map #(.getPath %) (walk root glob)))
 
