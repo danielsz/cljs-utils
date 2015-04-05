59685a31d63426f12793ab7ee0014c3742050850
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-04-05 14:19:32 +0300

    whitespace

--- src/cljs_utils/core.clj
+++ src/cljs_utils/core.clj
@@ -9,10 +9,8 @@
   (doall (filter #(re-matches pattern (.getName %))
                  (file-seq (file dirpath)))))
 
-;; usage: (slurp-glob "resources/stations" #".*\.edn"))
 (defn slurp-glob [root glob]
   (mapv #(edn/read-string (slurp (.getPath %))) (walk root glob)))
 
 (defmacro slurp-edn [root]
   (slurp-glob root #".*\.edn"))
-
