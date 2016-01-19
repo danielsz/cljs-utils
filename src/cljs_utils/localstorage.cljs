e6cef5cb11bb347707163eb0ade7c1fd9a592d4c
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-01-19 21:09:33 +0200

    Localstorage utils

---
+++ src/cljs_utils/localstorage.cljs
@@ -0,0 +1,29 @@
+(ns cljs-utils.localstorage)
+
+(defn set-item!
+  "Set `key' in browser's localStorage to `val`."
+  [key val]
+  (.setItem (.-localStorage js/window) key val))
+
+(defn get-item
+  "Returns value of `key' from browser's localStorage."
+  [key]
+  (.getItem (.-localStorage js/window) key))
+
+(defn remove-item!
+  "Remove the browser's localStorage value for the given `key`"
+  [key]
+  (.removeItem (.-localStorage js/window) key))
+
+(defn set-json-item!
+  "Set `key' in browser's localStorage to `val`."
+  [key val]
+  (let [item (.stringify js/JSON (clj->js val))]
+    (.setItem (.-localStorage js/window) key item)))
+
+(defn get-json-item!
+  "Returns value of `key' from browser's localStorage."
+  [key]
+  (let [item (.getItem (.-localStorage js/window) key)]
+    (clojure.walk/keywordize-keys (js->clj (.parse js/JSON item)))))
+
