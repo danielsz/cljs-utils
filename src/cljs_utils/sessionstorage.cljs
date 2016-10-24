fcddd823b7a5efe0a36d62d8e2bbd197ade1d4bc
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-10-24 05:22:04 +0300

    Session Storage
    - Mirror of localStorage

---
+++ src/cljs_utils/sessionstorage.cljs
@@ -0,0 +1,29 @@
+(ns cljs-utils.sessionstorage)
+
+(defn set-item!
+  "Set `key' in browser's sessionStorage to `val`."
+  [key val]
+  (.setItem (.-sessionStorage js/window) key val))
+
+(defn get-item
+  "Returns value of `key' from browser's sessionStorage."
+  [key]
+  (.getItem (.-sessionStorage js/window) key))
+
+(defn remove-item!
+  "Remove the browser's sessionStorage value for the given `key`"
+  [key]
+  (.removeItem (.-sessionStorage js/window) key))
+
+(defn set-json-item!
+  "Set `key' in browser's sessionStorage to `val`."
+  [key val]
+  (let [item (.stringify js/JSON (clj->js val))]
+    (.setItem (.-sessionStorage js/window) key item)))
+
+(defn get-json-item!
+  "Returns value of `key' from browser's sessionStorage."
+  [key]
+  (let [item (.getItem (.-sessionStorage js/window) key)]
+    (clojure.walk/keywordize-keys (js->clj (.parse js/JSON item)))))
+
