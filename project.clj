130ab6f5d9da9248be08199a53476d04c0069e59
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2021-03-21 17:33:45 +0200

    Migrated to meyvn
    - Hicada compiler

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject org.danielsz/cljs-utils "0.1.1"
-  :description "General purpose utils for clojurescript development."
-  :url "https://github.com/danielsz/cljs-utils"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :scm {:name "git"
-        :url "https://github.com/danielsz/cljs-utils"})
---
+++ src/cljs_utils/compilers/hicada.clj
@@ -0,0 +1,10 @@
+(ns cljs-utils.compilers.hicada
+  (:require [hicada.compiler]))
+
+(defmacro html
+  [body]
+  (hicada.compiler/compile body {:create-element 'js/React.createElement
+                                 :transform-fn (comp)
+                                 :array-children? false}
+                                 {} &env))
+
